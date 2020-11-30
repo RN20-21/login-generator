0a19ff7de968026a374b2edcd0c3ef543e904ca0
Raph <rafae@gmx.fr>
2020-11-30 10:23:19 +0100

    test passwordGenerator

--- src/test/java/geco/PasswordGenerationTest.java
+++ src/test/java/geco/PasswordGenerationTest.java
@@ -1,4 +1,25 @@
 package geco;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class PasswordGenerationTest {
+    private PasswordGeneration pg;
+    @Before
+    public void constructor(){
+        pg = new PasswordGeneration();
+    }
+
+    @Test
+    public void PasswordGenerationConstructorTest(){
+        assertNotNull(pg);
+    }
+
+    @Test
+    public void getRandomPasswordTest(){
+        assertEquals(8,pg.getRandomPassword().length());
+    }
 }
